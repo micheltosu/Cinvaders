53920fd304d418aa332486e0fc7324c2b8f596d6
Michel Tosu <michel@tosu.se>
2018-12-11 14:39:25 +0100

    testing gameengine

--- Cinvaders/Main.cpp
+++ Cinvaders/Main.cpp
@@ -1,42 +1,15 @@
-#ifdef _WIN32
-
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-
-#elif defined (__APPLE__)
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_mixer/SDL_mixer.h>
-#include <SDL2_ttf/SDL_ttf.h>
-#endif
-
 #include <string>
 #include <iostream>
+#include "GameEngine.h"
 
 using namespace std;
 int main(int argc, char** argv) {
 	int window_width = 1280;
 	int window_height = 720;
 
-
-	//Inititalizers
-	SDL_Init(SDL_INIT_EVERYTHING);
-	TTF_Init();
-
-	SDL_Window* window = SDL_CreateWindow("Titel", 100, 100, window_width, window_height, 0);
-	SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
-
-
+	ToMingine::GameEngine gm = ToMingine::GameEngine::getInstance();
 
 	SDL_Delay(15000);
 
-
-	//Destroyers
-	SDL_DestroyRenderer(render);
-	SDL_DestroyWindow(window);
-	TTF_Quit();
-	SDL_Quit();
 	return 0;
 }
