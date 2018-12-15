818e8f253c7b5ce632d9e97485fd319090c920a8
Tobbe <tobias.bjerking@gmail.com>
2018-12-15 01:51:54 +0100

    Add a map to Script that can store an input as Uint32 and a function.

--- Cinvaders/Script.h
+++ Cinvaders/Script.h
@@ -7,14 +7,34 @@
 #include <SDL2/SDL.h>
 #endif
 
+#include <map>
+
 namespace ToMingine {
 	class Script {
 	public:
 		virtual void run() {};
 		virtual void keyBoardEvent(SDL_Event& keyEvent) {};
+
+		template <typename RET, typename ARG>
+		void addInput(Uint32 key, RET(*fpek)(ARG)) {
+			input[key] = fpek;
+		}
+
+		template <typename RET>
+		void addInput(Uint32 key, RET(*fpek)()) {
+			input[key] = fpek;
+		}
+
+	void runInput(SDL_Event& key) {
+		input.find(key.key.keysym.sym);
+	}
+
+	private:
+		std::map<Uint32, void (*)()> input;
 	};
 
 }
+
 
 
 #endif
