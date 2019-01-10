d5c68385064f9358d68b6b85ad62492e6beb14dc
Michel Tosu <michel@tosu.se>
2019-01-10 20:31:26 +0100

    Revert "remove SDL  imports"
    This reverts commit b97adb2ca93d23a72235077788baadf14f17f87d.

--- Cinvaders/InputField.h
+++ Cinvaders/InputField.h
@@ -1,9 +1,20 @@
 #ifndef INPUTFIELD_H
 #define INPUTFIELD_H
 
+#ifdef _WIN32
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+
+#elif defined (__APPLE__)
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#endif
 
 #include "GameObject.h"
-#include "TextBox.h"
 
 namespace ToMingine {
 class Inputfield : public GameObject {
@@ -12,16 +23,13 @@ public:
     Inputfield(std::string path, int size, int x, int y, int w, int h);
     ~Inputfield();
     
-//    void draw();
-    
+    void draw();
 private:
+    TTF_Font* font;
     bool focus;
-    TextBox *box;
-    
     
     Inputfield(const Inputfield&) = delete;
     const Inputfield& operator=(const Inputfield&) = delete;
-    
 };
 }
 
--- Cinvaders/MouseManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MouseManager.cpp
-//  CINVADERS
-//
-//  Created by Michel Tosu on 2019-01-10.
-//  Copyright © 2019 Michel Tosu. All rights reserved.
-//
-
-#include "MouseManager.hpp"
