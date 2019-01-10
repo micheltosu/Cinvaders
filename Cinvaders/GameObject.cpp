be79df553f32261b6beec20df1b2e8c2237776b5
Michel Tosu <michel@tosu.se>
2019-01-10 12:55:09 +0100

    set to nullptr after delete

--- Cinvaders/GameObject.cpp
+++ Cinvaders/GameObject.cpp
@@ -24,6 +24,9 @@ namespace ToMingine {
 	GameObject::~GameObject() {
 		delete script;
 		delete renOb;
+        
+        script = nullptr;
+        renOb = nullptr;
 	}
 
     void GameObject::tick() {
