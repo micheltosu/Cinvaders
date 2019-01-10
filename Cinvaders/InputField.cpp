6e089f3947f247e0fdc5e9e742a938d85a3e0650
Michel Tosu <michel@tosu.se>
2019-01-10 09:58:08 +0100

    add input class

---
+++ Cinvaders/InputField.cpp
@@ -0,0 +1,2 @@
+#include "InputField.h"
+
---
+++ Cinvaders/InputField.h
@@ -0,0 +1,12 @@
+#ifndef INPUTFIELD_H
+#define INPUTFIELD_H
+
+#include "GameObject.h"
+
+namespace ToMingine {
+class InputField : public GameObject {
+    
+};
+}
+
+#endif /* INPUTFIELD_H */
