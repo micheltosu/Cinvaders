80e74b0e3f6e4afed7ea4895b6c6701e339e4578
Michel Tosu <michel@tosu.se>
2019-01-10 20:46:27 +0100

    add mouse manager class

---
+++ Cinvaders/MouseManager.cpp
@@ -0,0 +1,1 @@
+#include "MouseManager.h"
---
+++ Cinvaders/MouseManager.h
@@ -0,0 +1,10 @@
+#ifndef MOUSEMANAGER_H
+#define MOUSEMANAGER_H
+
+namespace ToMingine {
+    class MouseManager {
+        
+    }
+}
+
+#endif /* MouseManager_hpp */
