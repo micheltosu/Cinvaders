cab6be62674c06c86380ea6fb2b42b468a835600
Michel Tosu <michel@tosu.se>
2019-01-10 11:29:35 +0100

    add input field class

--- Cinvaders/InputField.cpp
+++ Cinvaders/InputField.cpp
@@ -1,2 +1,33 @@
-#include "InputField.h"
+#include "Inputfield.h"
 
+namespace ToMingine {
+    
+    Inputfield::Inputfield(std::string path, int size) : Inputfield(path, size, 100, 100, 200,50) {}
+    Inputfield::Inputfield(std::string path, int size, int x, int y, int w, int h) {
+        rect.x = x;
+        rect.y = y;
+        rect.w = w;
+        rect.y = y;
+        
+        font = TTF_OpenFont(path.c_str(), size);
+        
+        
+        
+        
+    }
+    
+    Inputfield::~Inputfield() {
+        delete font;
+        delete renOb;
+        
+        renOb = nullptr;
+        font = nullptr;
+    }
+    
+    void Inputfield::draw() {
+        if (renOb)
+            renOb->draw();
+            
+            
+    }
+}
--- Cinvaders/InputField.h
+++ Cinvaders/InputField.h
@@ -1,11 +1,35 @@
 #ifndef INPUTFIELD_H
 #define INPUTFIELD_H
 
+#ifdef _WIN32
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+
+#elif defined (__APPLE__)
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#endif
+
 #include "GameObject.h"
 
 namespace ToMingine {
-class InputField : public GameObject {
+class Inputfield : public GameObject {
+public:
+    Inputfield(std::string path, int size = 12);
+    Inputfield(std::string path, int size, int x, int y, int w, int h);
+    ~Inputfield();
     
+    void draw();
+private:
+    TTF_Font* font;
+    bool focus;
+    
+    Inputfield(const Inputfield&) = delete;
+    const Inputfield& operator=(const Inputfield&) = delete;
 };
 }
 
