ad9cefa6db13a391766d992b1c8d930d5e7dce2c
Michel Tosu <michel@tosu.se>
2019-01-17 22:19:57 +0100

    add main screen and first level

--- Cinvaders/Main.cpp
+++ Cinvaders/Main.cpp
@@ -10,67 +10,131 @@ using namespace std;
 using namespace ToMingine;
 
 int main(int argc, char** argv) {
-    int window_width = 1280;
-    int window_height = 720;
-
     GameEngine &ge = GameEngine::getInstance();
-    Scene myScene = Scene("Resources/Image/background.png");
-    Scene myScene2 = Scene("Resources/Image/background.png");
-    ge.addScene(&myScene);
-    ge.addScene(&myScene2);
+    
+    // Common things
+    SDL_Color transparent = {0,0,0,0};
+    SDL_Color fgColor = {71,255,39,0};
+    SDL_Color bgColor = {0,0,0,255};
+    
+    TextLabel *playerName = new TextLabel("Resources/Fonts/Pixeled.ttf",15, 25, 25, 0,0);
+    playerName->setBgColor(transparent);
+    playerName->setFgColor(fgColor);
+    playerName->setText("anonymous");
+    
+    
+    // Create scenes
+    Scene mainMenu = Scene("Resources/Image/bgMenu.png");
+    ge.addScene(&mainMenu);
+    
 
-	AnimatedSprite* playerSprite = new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1);
+    // Add main menu objects
+    TextLabel *gameLabel = new TextLabel("Resources/Fonts/Pixeled.ttf",40, 400, 50, 600, 100);
+    gameLabel->setBgColor(transparent);
+    gameLabel->setFgColor(fgColor);
+    gameLabel->setText("C INVADERS");
+    
+    TextLabel *tagline = new TextLabel("Resources/Fonts/Pixeled.ttf",20, 200, 200 , 0,0);
+    tagline->setBgColor(transparent);
+    tagline->setFgColor(fgColor);
+    tagline->setText("Defend your language against the invasion");
+    
+    TextLabel *nameInstructions = new TextLabel("Resources/Fonts/Pixeled.ttf",15, 200, 300, 0,0);
+    nameInstructions->setBgColor(transparent);
+    nameInstructions->setFgColor(fgColor);
+    nameInstructions->setText("Click in the box to enter player name:");
+    
+    Inputfield *nameInput = new Inputfield("Resources/Fonts/Pixeled.ttf", 15, 700,300,0,0);
+    nameInput->setFgColor(fgColor);
+    nameInput->setBgColor(bgColor);
+    nameInput->setText("Player 1");
+    
+    SDL_Rect buttonPos = {500,500,100,100};
+    auto updateNameLabel = []() -> void {
+        GameEngine::getInstance().nextScene();
+        
+    };
+    
+    Button *startButton = new Button("Resources/Fonts/Pixeled.ttf", "Click to play", updateNameLabel, buttonPos, 20);
+    startButton->setFgColor(fgColor);
+    startButton->setBgColor(bgColor);
+    
+    
+    TextLabel *instructions = new TextLabel("Resources/Fonts/Pixeled.ttf",15, 380, 650, 0,0);
+    instructions->setBgColor(transparent);
+    instructions->setFgColor(fgColor);
+    instructions->setText("Move with the WASD keys, shoot with SPACEBAR");
+    
+    
+    
+    mainMenu.addObject(gameLabel);
+    mainMenu.addObject(tagline);
+    mainMenu.addObject(nameInstructions);
+    mainMenu.addObject(startButton);
+    mainMenu.addObject(nameInput);
+    mainMenu.addObject(instructions);
+    
+    
+    //
+    // First level
+    //
+    
+    Scene level1 = Scene("Resources/Image/background.png");
+    ge.addScene(&level1);
+    level1.addObject(playerName);
+    
+    //Add Scene objects
+    AnimatedSprite* playerSprite = new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1);
+    
+    PhysicsObject* player = new PhysicsObject(playerSprite, PLAYER, 500, 550);
 	
-	PhysicsObject* player = new PhysicsObject(playerSprite, PLAYER, 500, 550);
-	RigidObject* enemy = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 400, 400);
-	RigidObject* enemy2 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 300, 200);
-	RigidObject* enemy3 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 600, 200);
-	RigidObject* enemy4 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 700, 200);
-	RigidObject* wall = new RigidObject(new AnimatedSprite("Resources/Image/wall.png", 5, 1), WALL, 0, 644);
-	RigidObject* sidewall = new RigidObject(new AnimatedSprite("Resources/Image/sidewall.png", 5, 1), WALL, 1, 0);
+
 
     PlayerScript* ps = new PlayerScript(player);
-	player->addScript(ps);
-	//enemy->addScript(new EnemyScript(enemy));
-	enemy2->addScript(new EnemyScript(enemy2));
-	enemy3->addScript(new EnemyScript(enemy3));
-	enemy4->addScript(new EnemyScript(enemy4));
-	myScene.addObject(player);
-	myScene.addObject(enemy);
-	/*myScene.addObject(enemy2);
-	myScene.addObject(enemy3);
-	myScene.addObject(enemy4);
-	myScene.addObject(wall);
-	myScene.addObject(sidewall);*/
+    player->addScript(ps);
+    MemberFunctionKeybinding<PlayerScript>* mk = new MemberFunctionKeybinding<PlayerScript>(ps, &PlayerScript::shoot);
+    level1.addObject(player);
+    
+    
+    RigidObject* enemy1 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 100, 50);
+    enemy1->addScript(new EnemyScript(enemy1));
+    level1.addObject(enemy1);
+    
+    RigidObject* enemy2 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 250, 100);
+    enemy2->addScript(new EnemyScript(enemy2));
+    level1.addObject(enemy2);
+    
+    RigidObject* enemy3 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 400, 50);
+    enemy3->addScript(new EnemyScript(enemy3));
+    level1.addObject(enemy3);
+    
+    RigidObject* enemy4 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 550, 100);
+    enemy4->addScript(new EnemyScript(enemy4));
+    level1.addObject(enemy4);
+    
+    RigidObject* enemy5 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 700, 50);
+    enemy5->addScript(new EnemyScript(enemy5));
+    level1.addObject(enemy5);
+    
+    RigidObject* enemy6 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 850, 100);
+    enemy6->addScript(new EnemyScript(enemy6));
+    level1.addObject(enemy6);
+    
+    RigidObject* enemy7 = new RigidObject(new AnimatedSprite("Resources/Image/blueShipSheet.png", 4, 1), ENEMY, 1000, 50);
+    enemy7->addScript(new EnemyScript(enemy7));
+    level1.addObject(enemy7);
+    
+    
+
 
     // Keybindings
     SDL_Keycode sk = SDLK_SPACE;
-    MemberFunctionKeybinding<PlayerScript>* mk = new MemberFunctionKeybinding<PlayerScript>(ps, &PlayerScript::shoot);
+
     GameEngine::getInstance().keyboardManager()->addBinding<PlayerScript>(sk, mk);
-    
     GameEngine::getInstance().keyboardManager()->addListener(player);
     // End keybindings
     
     
-    // TextBox
-    Inputfield* inField = new Inputfield("Resources/Fonts/Pixeled.ttf", 15, 100,100,200,50);
-    myScene.addObject(inField);
-    
-    // TextLabel
-    TextLabel* txtLbl = new TextLabel("Resources/Fonts/Pixeled.ttf", 20, 300, 75, 300, 50);
-    txtLbl->setText("C Invaders");
-    myScene.addObject(txtLbl);
-    
-    
-    //Button
-    SDL_Color fgColor = {71,255,39,255};
-    SDL_Color bgColor = {0,0,0,255};
-    auto buttonFunk = []()->void{ std::cout << "I WAS HIT!" << std::endl; };
-    SDL_Rect btnPos = {900,550,100,50};
-    Button btn("Resources/Fonts/Pixeled.ttf", "Click me!", buttonFunk ,btnPos);
-    btn.setFgColor(fgColor);
-    btn.setBgColor(bgColor);
-    myScene.addObject(&btn);
     
     ge.run();
     return 0;
